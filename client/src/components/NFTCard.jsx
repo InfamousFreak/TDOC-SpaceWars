a1b8ba8483d8d7d6f8949bd6ea856846e4b3059c
Arkaprava Dhar <raj2003dhar@gmail.com>
2024-12-12 18:41:47 +0530

    List nft modal change

--- client/src/components/NFTCard.jsx
+++ client/src/components/NFTCard.jsx
@@ -2,28 +2,30 @@ import React, { useState } from "react";
 import { useGlobalContext } from "../context";
 
 const NFTCard = ({ nft }) => {
-
   const { contracts, accounts } = useGlobalContext();
 
   const [isListModalOpen, setIsListModalOpen] = useState(false);
   const [price, setPrice] = useState(0);
 
   const handleListNFT = () => {
-
     console.log(price);
     console.log(contracts.NFT_MarketPlace._address);
 
-    if(price<=0) return;
+    if (price <= 0) return;
 
     const listNFT = async () => {
-      await contracts.NFT_MarketPlace.methods.approve(contracts.NFT_MarketPlace._address, nft.tokenId).send({ from: accounts[0] });
+      await contracts.NFT_MarketPlace.methods
+        .approve(contracts.NFT_MarketPlace._address, nft.tokenId)
+        .send({ from: accounts[0] });
       // await contracts.NFT_MarketPlace.methods.setApprovalForAll(contracts.NFT_MarketPlace._address, true).send({ from: accounts[0]});
-      const result = await contracts.NFT_MarketPlace.methods.listNFT(nft.tokenId, price).send({ from: accounts[0] });
+      const result = await contracts.NFT_MarketPlace.methods
+        .listNFT(nft.tokenId, price)
+        .send({ from: accounts[0] });
       console.log(result);
-    }
+    };
 
     listNFT();
-  }
+  };
 
   return (
     <div className="w-64 p-4 rounded-lg bg-gradient-to-b from-gray-700 via-gray-800 to-gray-900 shadow-lg">
@@ -49,28 +51,29 @@ const NFTCard = ({ nft }) => {
       </div>
 
       {isListModalOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-          <div className="bg-gray-900 p-6 rounded-lg shadow-lg w-11/12 md:w-1/2 h-full flex flex-col items-center justify-evenly">
-            <h2 className="text-3xl font-semibold text-center text-gray-200 mb-6">Select Price:</h2>
+        <div className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50">
+          <div className="bg-gray-800 p-6 rounded-lg shadow-lg text-center w-11/12 md:w-1/3">
+            <h3 className="text-2xl font-bold mb-4">Select Price</h3>
             <input
-              className="p-3 rounded-lg bg-gray-700 text-white"
+              className="p-3 w-full rounded-md bg-gray-700 text-white text-lg mb-6"
               type="number"
               value={price}
+              placeholder="Enter price"
               onChange={(e) => setPrice(e.target.value)}
             />
-            <div className="flex justify-end gap-4">
-            <button
-              className="p-3 rounded-lg bg-gray-600 hover:bg-gray-700 text-white font-bold"
-              onClick={() => {setIsListModalOpen(false)}}
-            >
-              Cancel
-            </button>
-            <button
-              className="p-3 rounded-lg bg-blue-500 hover:bg-blue-600 text-white font-bold"
-              onClick={handleListNFT}
-            >
-              List For Sale
-            </button>
+            <div className="flex gap-4 justify-center mt-4">
+              <button
+                onClick={() => setIsListModalOpen(false)}
+                className="px-6 py-2 w-36 bg-gradient-to-r from-red-500 to-pink-500 rounded-lg text-white font-bold hover:from-pink-500 hover:to-red-500 transition-all"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={handleListNFT}
+                className="px-6 py-2 w-36 bg-gradient-to-r from-green-500 to-blue-500 rounded-lg text-white font-bold hover:from-blue-500 hover:to-green-500 transition-all"
+              >
+                List For Sale
+              </button>
             </div>
           </div>
         </div>
