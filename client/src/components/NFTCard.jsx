9df107ebaa1b368d896891e28576ea61c8ea81a6
Arkaprava Dhar <raj2003dhar@gmail.com>
2024-12-13 01:35:25 +0530

    pop up on skin or background change

--- client/src/components/NFTCard.jsx
+++ client/src/components/NFTCard.jsx
@@ -1,23 +1,30 @@
-import React, { useState , useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useGlobalContext } from "../context";
 import "../index.css";
 
 const NFTCard = ({ nft }) => {
-  const { contracts, accounts, setActiveSkin , activeSkin, setActiveBackground, activeBackground} = useGlobalContext();
+  const { contracts, accounts, setActiveSkin, activeSkin, setActiveBackground, activeBackground } = useGlobalContext();
 
   const [isListModalOpen, setIsListModalOpen] = useState(false);
   const [price, setPrice] = useState(0);
+  const [isChangesApplied, setIsChangesApplied] = useState(false); // For managing the pop-up visibility
 
   const handleChange = () => {
-    if(nft.type == "spaceship") 
-      setActiveSkin(nft.image); 
-    else 
-      setActiveBackground(nft.image);
+    if (nft.type === "spaceship") setActiveSkin(nft.image);
+    else setActiveBackground(nft.image);
+
+    // Show the pop-up after applying changes
+    setIsChangesApplied(true);
+
+    // Hide the pop-up after 3 seconds
+    setTimeout(() => {
+      setIsChangesApplied(false);
+    }, 3000);
   };
 
   useEffect(() => {
     console.log("Updated activeSkin:", activeSkin);
-  }, [activeSkin]); 
+  }, [activeSkin]);
 
   const handleListNFT = () => {
     console.log(price);
@@ -29,7 +36,6 @@ const NFTCard = ({ nft }) => {
       await contracts.NFT_MarketPlace.methods
         .approve(contracts.NFT_MarketPlace._address, nft.tokenId)
         .send({ from: accounts[0] });
-      // await contracts.NFT_MarketPlace.methods.setApprovalForAll(contracts.NFT_MarketPlace._address, true).send({ from: accounts[0]});
       const result = await contracts.NFT_MarketPlace.methods
         .listNFT(nft.tokenId, price)
         .send({ from: accounts[0] });
@@ -44,7 +50,6 @@ const NFTCard = ({ nft }) => {
       <div className="w-fill h-40 md:h-60 flex items-center justify-center bg-gray-600 rounded-md overflow-hidden">
         <img
           src={nft.image}
-          // src='../../public/vite.svg'
           alt={nft.name}
           className="object-cover w-full h-full"
         />
@@ -93,6 +98,15 @@ const NFTCard = ({ nft }) => {
                 List For Sale
               </button>
             </div>
+          </div>
+        </div>
+      )}
+      
+      {isChangesApplied && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+          <div className="bg-gray-800 p-6 rounded-lg shadow-lg text-center w-11/12 md:w-1/3">
+            <h3 className="text-xl font-semibold text-green-500">Changes Applied!</h3>
+            <p className="text-sm text-gray-300 mt-2">Your spaceship or background has been updated.</p>
           </div>
         </div>
       )}
